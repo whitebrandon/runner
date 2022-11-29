80243b3cca938db53500d1a1c639941a938793c7
Brandon White <white.brandonsean@gmail.com>
2022-11-29 18:06:08 -0500

    Added player surface and rectangle

--- my_game.py
+++ my_game.py
@@ -13,6 +13,10 @@ ground_surface = pygame.image.load('ultimatepygameintro/graphics/ground.png').co
 text_surface = test_font.render('My game', False, 'Black')
 snail_surface = pygame.image.load('ultimatepygameintro/graphics/snail/snail1.png').convert_alpha()
 snail_x_pos = 600
+
+player_surf = pygame.image.load('ultimatepygameintro/graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80, 300))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -22,10 +26,12 @@ while True:
     screen.blit(sky_surface, (0,0)) # blit stands for [bl]ock [i]mage [t]ransfer
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (350, 100))
+    snail_rect = snail_surface.get_rect(midbottom = (snail_x_pos, 300))
     snail_x_pos -= 4
-    if snail_x_pos < -4:
+    if snail_x_pos < -100:
         snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos, 250))
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surf, player_rect)
     # update everything
     pygame.display.update()
     fps.tick(45)
